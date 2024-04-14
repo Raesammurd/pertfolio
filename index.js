5ec545dfe786e9703c1244b67a620e9672c0e57c
Raesammurd <olowookererereloluwa@gmail.com>
2024-04-14 10:24:52 -0800

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// Skills
-// $('.skills').waypoint(function () {
-//   $('.progress .progress-bar').each(function () {
-//       $(this).css("width", $(this).attr("aria-valuenow") + '%');
-//   });
-// }, {offset: '80%'});
 
 
-const progressBars = document.querySelectorAll(".progress-bar");
 
-progressBars.forEach(progressBar => {
-  const progressValue = progressBar.parentNode.previousElementSibling.querySelector("p:last-child").textContent.trim();
-  progressBar.style.width = progressValue + "%";
-});
