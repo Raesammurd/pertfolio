917887d71054b679f1cead78951a4d2d9c2ae3fe
Raesammurd <olowookererereloluwa@gmail.com>
2024-04-15 01:54:25 -0800

    sixth commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault(); // Prevent default scrolling
+
+      const targetSection = document.getElementById(this.getAttribute('href').slice(1));
+      targetSection.scrollIntoView({ behavior: 'smooth' });  // Smooth scrolling
+    });
+  });
+
 
 
